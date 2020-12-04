e9b5691692cd8ab267c75a3c78035900f72ae8c2
tkonopka <tokonopka@gmail.com>
2020-12-04 10:38:25 +0000

    data archiving script, tiff/png generating script

---
+++ archive.bash
@@ -0,0 +1,5 @@
+#!/bin/bash -l
+
+zip -r pan-ontology-ml-archive.zip data/instances* data/obo* data/owl* data/wiktionary* data/ols*
+
+
--- vignettes/figures.bash
+++ vignettes/figures.bash
@@ -7,11 +7,14 @@ for figname in all casestudies explaining ontologies reconstruction;
 do
   gs -dNOPAUSE -r600x600 -q -sDEVICE=tiffscaled24 -dBATCH -sCompression=lzw \
      -sOutputFile=Fig-$figname.tiff fig.$figname-1.pdf
+  gs -dNOPAUSE -r600x600 -q -sDEVICE=png16m -dBATCH \
+     -sOutputFile=Fig-$figname.png fig.$figname-1.pdf
 done
 
 
 cd ../../
-mkdir tiff
-mv OntoML_files/figure-latex/*tiff tiff/
+mkdir figures
+mv OntoML_files/figure-latex/*tiff figures/
+mv OntoML_files/figure-latex/*png figures/
 
 
